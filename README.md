13b769c3451a329311139a60c3e9a6e94d0b8253
Chetan Satpute <chetan.satpute2002@gmail.com>
2024-06-15 00:45:43 +0530

    CS-2: Project setup (#2)
    * created a vite project
    
    a vite project was initialised using the command
    
    npm create vite@latest
    
    * Added google typescript style
    
    - https://github.com/google/gts
    
    * Project cleanup and added tailwind css
    
    - removed vite code template
    - installed and configured tailwind css
    
    * prettier for tailwind css plugin
    
    - fixed build folder name in eslintignore

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Code canvas app
+
+[![Code Style: Google](https://img.shields.io/badge/code%20style-google-blueviolet.svg)](https://github.com/google/gts)
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h1>Code Canvas</h1>
+    </div>
+  );
+}
+
+export default App;
