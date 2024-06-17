9f5aadbb8b1e8ffda67b439e2360d67f0965f583
Chetan Satpute <chetan.satpute2002@gmail.com>
2024-06-17 10:59:00 +0530

    Tonal get started button on home screen

--- src/routes/HomeScreen.tsx
+++ src/routes/HomeScreen.tsx
@@ -1,13 +1,20 @@
+import '@material/web/button/filled-tonal-button';
+
 function HomeScreen() {
   return (
-    <div className="flex h-screen w-screen bg-background">
-      <div className="flex flex-1 flex-col items-center justify-center bg-blue-200 p-2 lg:items-start lg:pl-8">
-        <h1 className="mb-4 font-salsa text-4xl">Code canvas</h1>
-        <p className="text-center font-kalam text-xl lg:text-left">
+    <div className="flex h-screen w-screen bg-surface">
+      <div className="flex flex-1 flex-col items-center justify-center p-2 lg:items-start lg:pl-8">
+        <h1 className="mb-4 font-salsa text-4xl text-on-surface">
+          Code canvas
+        </h1>
+        <p className="mb-4 text-center font-kalam text-xl text-on-surface lg:text-left">
           Interactive Exploration of Algorithms & Data Structures
         </p>
+        <md-filled-tonal-button class="font-salsa">
+          Get started
+        </md-filled-tonal-button>
       </div>
-      <div className="hidden flex-1 bg-blue-500 lg:block"></div>
+      <div className="hidden flex-1 lg:block"></div>
     </div>
   );
 }
