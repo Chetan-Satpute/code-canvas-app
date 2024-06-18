b7b2ee9a6dc8c9edb00b9133517cb19ed95861ed
Chetan Satpute <chetan.satpute2002@gmail.com>
2024-06-18 06:02:36 +0530

    Canvas interaction

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,14 @@
+import {CanvasEdge} from './canvas/edge';
+import {CanvasLabel} from './canvas/label';
+import {CanvasNode} from './canvas/node';
+
 export interface Point {
   x: number;
   y: number;
+}
+
+export interface Frame {
+  nodes: CanvasNode[];
+  edges: CanvasEdge[];
+  labels: CanvasLabel[];
 }
