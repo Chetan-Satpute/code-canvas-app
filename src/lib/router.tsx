dc76c7c919c837258e77b423cb45b0b324f710bf
Chetan Satpute <chetan.satpute2002@gmail.com>
2024-07-14 23:02:52 +0530

    Using hash router to support github pages

--- src/lib/router.tsx
+++ src/lib/router.tsx
@@ -1,36 +1,33 @@
-import {createBrowserRouter} from 'react-router-dom';
+import {createHashRouter} from 'react-router-dom';
 import HomeScreen from '../routes/HomeScreen';
 import CanvasScreen from '../routes/CanvasScreen';
 import AlgorithmPanel from '../routes/CanvasScreen/AlgorithmPanel';
 import StructurePanel from '../routes/CanvasScreen/StructurePanel';
 import ErrorScreen from '../routes/ErrorScreen';
 
-const router = createBrowserRouter(
-  [
-    {
-      path: 'something-went-wrong',
-      element: <ErrorScreen />,
-    },
-    {
-      path: '/',
-      element: <HomeScreen />,
-    },
-    {
-      path: '/:structureId',
-      element: <CanvasScreen />,
-      children: [
-        {
-          path: '',
-          element: <StructurePanel />,
-        },
-        {
-          path: ':algorithmId',
-          element: <AlgorithmPanel />,
-        },
-      ],
-    },
-  ],
-  {basename: '/code-canvas'}
-);
+const router = createHashRouter([
+  {
+    path: 'something-went-wrong',
+    element: <ErrorScreen />,
+  },
+  {
+    path: '/',
+    element: <HomeScreen />,
+  },
+  {
+    path: '/:structureId',
+    element: <CanvasScreen />,
+    children: [
+      {
+        path: '',
+        element: <StructurePanel />,
+      },
+      {
+        path: ':algorithmId',
+        element: <AlgorithmPanel />,
+      },
+    ],
+  },
+]);
 
 export default router;
