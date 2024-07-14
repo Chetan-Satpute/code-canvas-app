097bb3ab2722289caa106b11826cfa2100d997e6
Chetan Satpute <chetan.satpute2002@gmail.com>
2024-07-14 15:44:29 +0530

    Configured base name code-canvas

--- src/lib/router.tsx
+++ src/lib/router.tsx
@@ -5,29 +5,32 @@ import AlgorithmPanel from '../routes/CanvasScreen/AlgorithmPanel';
 import StructurePanel from '../routes/CanvasScreen/StructurePanel';
 import ErrorScreen from '../routes/ErrorScreen';
 
-const router = createBrowserRouter([
-  {
-    path: 'something-went-wrong',
-    element: <ErrorScreen />,
-  },
-  {
-    path: '/',
-    element: <HomeScreen />,
-  },
-  {
-    path: '/:structureId',
-    element: <CanvasScreen />,
-    children: [
-      {
-        path: '',
-        element: <StructurePanel />,
-      },
-      {
-        path: ':algorithmId',
-        element: <AlgorithmPanel />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: 'something-went-wrong',
+      element: <ErrorScreen />,
+    },
+    {
+      path: '/',
+      element: <HomeScreen />,
+    },
+    {
+      path: '/:structureId',
+      element: <CanvasScreen />,
+      children: [
+        {
+          path: '',
+          element: <StructurePanel />,
+        },
+        {
+          path: ':algorithmId',
+          element: <AlgorithmPanel />,
+        },
+      ],
+    },
+  ],
+  {basename: '/code-canvas'}
+);
 
 export default router;
