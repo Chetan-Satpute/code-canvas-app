1eeb76d62e89c3084c4c872b7c7860b010d0c4f0
Chetan Satpute <chetan.satpute2002@gmail.com>
2024-07-21 15:32:06 +0530

    changed router to browser router

--- src/lib/router.tsx
+++ src/lib/router.tsx
@@ -1,33 +1,36 @@
-import {createHashRouter} from 'react-router-dom';
+import {createBrowserRouter} from 'react-router-dom';
 import HomeScreen from '../routes/HomeScreen';
 import CanvasScreen from '../routes/CanvasScreen';
 import AlgorithmPanel from '../routes/CanvasScreen/AlgorithmPanel';
 import StructurePanel from '../routes/CanvasScreen/StructurePanel';
 import ErrorScreen from '../routes/ErrorScreen';
 
-const router = createHashRouter([
-  {
-    path: 'something-went-wrong',
-    element: <ErrorScreen />,
-  },
-  {
-    path: '/',
-    element: <HomeScreen />,
-  },
-  {
-    path: '/:structureId',
-    element: <CanvasScreen />,
-    children: [
-      {
-        path: '',
-        element: <StructurePanel />,
-      },
-      {
-        path: ':algorithmId',
-        element: <AlgorithmPanel />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: 'something-went-wrong',
+      element: <ErrorScreen />,
+    },
+    {
+      path: '/',
+      element: <HomeScreen />,
+    },
+    {
+      path: '/:structureId',
+      element: <CanvasScreen />,
+      children: [
+        {
+          path: '',
+          element: <StructurePanel />,
+        },
+        {
+          path: ':algorithmId',
+          element: <AlgorithmPanel />,
+        },
+      ],
+    },
+  ],
+  {basename: '/code-canvas'}
+);
 
 export default router;
